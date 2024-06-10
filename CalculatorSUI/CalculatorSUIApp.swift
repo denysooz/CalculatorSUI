8fbfa1d584fe64a7734ce465721fe2510305560e
denysooz <den.darevskiy@mail.ru>
2024-06-10 14:39:58 +0300

    Initial Commit

---
+++ CalculatorSUI/CalculatorSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSUIApp.swift
+//  CalculatorSUI
+//
+//  Created by Denis Dareuskiy on 10.06.24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
